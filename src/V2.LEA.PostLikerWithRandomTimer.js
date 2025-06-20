f8a4670b3358d909b78ea77f139db895ac883180
Amin Boulouma <james@boulouma.com>
2025-06-20 11:02:20 +0200

    Update V2.LEA.PostLikerWithRandomTimer.js

--- src/V2.LEA.PostLikerWithRandomTimer.js
+++ src/V2.LEA.PostLikerWithRandomTimer.js
@@ -1,11 +1,12 @@
 // Get all the elements with the specified class
 const elements = document.getElementsByClassName("flex-wrap justify-center artdeco-button__text align-items-center");
 
-// Function to simulate clicking with random delay
+// Function to simulate clicking with random delay and logging
 function clickWithDelay(index) {
   if (index >= elements.length) return;
 
   elements[index].click();
+  console.log(`Liked post #${index + 1}`);
 
   const delay = Math.floor(Math.random() * 1000) + 500; // 500ms to 1500ms
 
